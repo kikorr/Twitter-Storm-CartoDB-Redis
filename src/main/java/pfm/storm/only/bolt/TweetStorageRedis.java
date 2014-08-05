5eb3ee03607c863cc902a2f1f87f23516c8d0259
Rodrigo de la Orden <rdorden@gmail.com>
2014-08-05 22:16:07 +0200

    First Commit
    First Commit about PFM Experto en Big Data

---
+++ src/main/java/pfm/storm/only/bolt/TweetStorageRedis.java
@@ -0,0 +1,43 @@
+package pfm.storm.only.bolt;
+
+import java.util.Map;
+
+import redis.clients.jedis.Jedis;
+import twitter4j.Status;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.BasicOutputCollector;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseBasicBolt;
+import backtype.storm.tuple.Tuple;
+
+public class TweetStorageRedis extends BaseBasicBolt {
+
+	private static final long serialVersionUID = 1L;
+	transient Jedis jedis; 
+	private String redisHost;
+	private int redisPort;
+
+	public TweetStorageRedis(String redisHost, int redisPort) {
+		this.redisHost = redisHost;
+		this.redisPort = redisPort;
+	}
+
+	@Override
+	public void execute(Tuple input, BasicOutputCollector collector) {
+		Status tweet = (Status)input.getValueByField("tweet");
+		jedis.hset("tweet:hashtags", String.valueOf(tweet.getId()), tweet.getText());
+	}
+
+	@Override
+	public void prepare(@SuppressWarnings("rawtypes") Map stormConf, TopologyContext context) {
+		jedis = new Jedis(redisHost, redisPort, 1800);
+		jedis.connect();
+		super.prepare(stormConf, context);
+	}
+	
+	@Override
+	public void declareOutputFields(OutputFieldsDeclarer declarer) {
+	}
+	
+
+}
